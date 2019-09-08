a81fbd4a03822812e43006636fb872778c794e66
THAVASIGTI <ganeshanthavasigti1032000@gmail.com>
2019-09-08 06:16:48 +0530

    gust for node

--- loggingFile.py
+++ loggingFile.py
@@ -3,22 +3,27 @@ import zipfile
 
 def main():
 	path = os.getcwd()
-	dest = os.path.join(path,"comp")
-	try:
-		os.mkdir(dest)
-	except OSError:
-		print("aldery use")
+	#dest = os.path.join(path,"comp")
+	#try:
+	#	os.mkdir(dest)
+	#except OSError:
+	#	print("aldery use")
 
 	log = os.path.join(path,"l.log")
 	#print(log)
+	l = os.path.basename(log)
+	print(l)
 	vm = os.path.join(path,"vm.log")
 #	#print(vm)
-	shutil.copy(log,dest)
-	shutil.copy(vm,dest)
-	d = os.path.basename(dest)
+	v = os.path.basename(vm)
+	print(v)
+#	shutil.copy(log,dest)
+#	shutil.copy(vm,dest)
+#	d = os.path.basename(dest)
 	comp = "/home/thavasigti/snap/compress"
 
-	os.popen("tar -zcvf "+comp+".tar "+d) 
+	os.popen("tar -zcvf "+comp+".tar "+l+" "+v)
+#	os.popen("tar -zcvf "+comp+".tar "+v) 
 #	with ZipFile.ZipFile(comp,"w")
 #    	j.write(log)
 #    	j.write(vm)
